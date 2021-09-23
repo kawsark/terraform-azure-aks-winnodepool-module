1ad264d8188839c6fc1452c6750fb8a2cd6d710e
kawsark <kawsar@hashicorp.com>
2021-09-23 17:33:17 -0400

    added module code

---
+++ 06-aks-administrators-azure-ad.tf
@@ -0,0 +1,31 @@
+data "azuread_client_config" "current" {}
+
+
+# Create Azure AD Group in Active Directory for AKS Admins
+resource "azuread_group" "aks_administrators" {
+  display_name     = "${azurerm_resource_group.aks_rg.name}-cluster-administrators"
+  owners           = [data.azuread_client_config.current.object_id]
+  # security_enabled = true
+  description      = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-cluster."
+}
+
+# To fetech current user data
+#data "azuread_user" "azuread_existing_user" {
+#  user_principal_name = "satish.kamasamudram@atyeti.com"
+#}
+
+# link existing user to the new group
+#resource "azuread_group_member" "link_existing_user" {
+#  group_object_id  = azuread_group.aks_administrators.id
+#  member_object_id = data.azuread_user.azuread_existing_user.id
+#}
+
+# For creating new users - we need the below resource block. Left the below block for your reference
+/*
+resource "azuread_user" "example" {
+  user_principal_name = "jdoe@hashicorp.com"
+  display_name        = "J. Doe"
+  mail_nickname       = "jdoe"
+  password            = "SecretP@sswd99!"
+}
+*/
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # terraform-azure-aks-winnodepool-module
+This module can be used to provision an AKS cluster with a Linux and Windows Node Pool. 
 
+## Example usage
+- Write a main.tf file to use this module
+```bash
+
+```
+- Set ARM_* environment variables for Azure credentials
+- Run terraform plan and apply
+```bash
+terraform plan
+terraform apply
+``` 
+- Use the kubeconfig output to perform kubectl
+```bash
+```
+
+## Inputs
+Please see the [02-variables.tf](./02-variables.tf) file for inputs. 
+
+## Outputs
+Please see the [08-outputs.tf](./08-outputs.tf) file for outputs.
