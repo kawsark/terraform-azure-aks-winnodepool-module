e13b9f212bd5af76c31a6d5901aa4b603dfe658b
Neil Dahlke <neil.dahlke@gmail.com>
2021-09-29 19:32:38 -0700

    Break everything down into re-usable functions.

--- scripts/python/main.py
+++ scripts/python/main.py
@@ -4,14 +4,60 @@ import hvac
 import os
 import json
 
+# TODO: get the tfc token from Vault
 TFC_TOKEN = os.getenv("TFC_TOKEN", None)
 TFC_URL = os.getenv("TFC_URL", None)  # ex: https://app.terraform.io
 
 VAULT_ADDR = os.getenv("VAULT_ADDR", "http://localhost:8200")
 VAULT_TOKEN = os.getenv("VAULT_TOKEN", None)
 
+# TODO: have two separate ones or how to handle with the subscription generation?
 VAULT_AZURE_PRIV_ROLE="my-role"
 VAULT_AZURE_UNPRIV_ROLE="my-role"
+
+
+def get_azure_creds(vault_client, vault_azure_role):
+	# TODO: take the role as a variable
+	azure_config = vault_client.secrets.azure.read_config()
+	azure_creds = vault_client.secrets.azure.generate_credentials(name=vault_azure_role)
+
+	subscription_id = azure_config["subscription_id"]
+	tenant_id = azure_config["tenant_id"]
+	client_id = azure_creds["client_id"]
+	client_secret = azure_creds["client_secret"]
+
+	merged_creds_data = [
+		{
+			"key": "ARM_SUBSCRIPTION_ID",
+			"value": subscription_id,
+			"sensitive": False,
+			"hcl": False,
+			"category": "env"
+		},
+		{
+			"key": "ARM_TENANT_ID",
+			"value": tenant_id,
+			"sensitive": True,
+			"hcl": False,
+			"category": "env"
+		},
+		{
+			"key": "ARM_CLIENT_ID",
+			"value": client_id,
+			"sensitive": True,
+			"hcl": False,
+			"category": "env"
+		},
+		{
+			"key": "ARM_CLIENT_SECRET",
+			"value": client_secret,
+			"sensitive": True,
+			"hcl": False,
+			"category": "env"
+		},
+	]
+
+	return merged_creds_data
 
 
 def build_create_ws_payload(ws):
@@ -76,7 +122,7 @@ def build_create_run_payload(ws_id):
 	}
 
 
-def create_privileged_ws(ws_config, tfc_client, vault_client):
+def create_ws(ws_config, tfc_client, vault_client):
 	# TODO: break down this function into multiple functions that can be re-used
 	org_name = ws_config["org_name"]
 	ws_name = ws_config["ws_name"]
@@ -88,9 +134,9 @@ def create_privileged_ws(ws_config, tfc_client, vault_client):
 		workspace = tfc_client.workspaces.show(workspace_name=ws_name)
 		# NOTE: only use this when not auto-applying, as then we may have orphaned resources
 		# TODO: remove this when I'm not iterating, as this deletes the workspace
-		# print(f"Workspace {ws_name} found, deleting it while iterating...")
-		# tfc_client.workspaces.destroy(workspace_name=ws_name)
-		workspace = None
+		print(f"Workspace {ws_name} found, deleting it while iterating...")
+		tfc_client.workspaces.destroy(workspace_name=ws_name)
+		# workspace = None
 	except TFCHTTPNotFound:
 		print(f"Workspace {ws_name} not found.")
 
@@ -99,96 +145,36 @@ def create_privileged_ws(ws_config, tfc_client, vault_client):
 		create_ws_payload = build_create_ws_payload(ws_config)
 		workspace = tfc_client.workspaces.create(create_ws_payload)
 
-	ws_id = workspace["data"]["id"]
+	return workspace
 
+
+def populate_tf_vars(ws_id, ws_config):
 	# Once the workspace is created, inject the user defined variables
 	# TODO: try catch on the variables if they already exist?
 	for var in ws_config["variables"]:
 		create_var_payload = build_create_var_payload(ws_id, var)
 		tfc_client.workspace_vars.create(ws_id, create_var_payload)
 
+
+def populate_env_vars(ws_id, vault_azure_role):
 	# Once the workspace is created, get the variables that are needed from Vault
 	print("Getting Azure Creds from Vault, this may take a moment...")
-	azure_creds_vars = get_azure_creds(vault_client)
-	print("Azure Creds retrieved from Vault")
+	# TODO: the creds don't have enough privileges to create
+	azure_creds_vars = get_azure_creds(vault_client, vault_azure_role)
+	print("Azure Creds retrieved from Vault.")
 
 	# Inject the credentials into the workspace
 	for var in azure_creds_vars:
 		create_var_payload = build_create_var_payload(ws_id, var)
 		tfc_client.workspace_vars.create(ws_id, create_var_payload)
 
+
+def trigger_run(ws_id):
 	# Trigger a run on the workspace
 	create_run_payload = build_create_run_payload(ws_id)
 	created_run = tfc_client.runs.create(create_run_payload)
 	print(created_run)
-
 	# Wait for the run to complete, then create the next workspace
-
-
-def create_unprivileged_ws(ws_config, tfc_client):
-	org_name = ws_config["org_name"]
-	ws_name = ws_config["ws_name"]
-
-	tfc_client.set_org(org_name)
-
-	existing_ws = None
-	try:
-		existing_ws = tfc_client.workspaces.show(workspace_name=ws_name)
-	except TFCHTTPNotFound as notfound:
-		print(f"Workspace {ws_name} not found.")
-
-	if existing_ws is None:
-		print(f"Creating workspace {ws_name}")
-		create_ws_payload = build_create_ws_payload(ws_config)
-		created_ws = tfc_client.workspaces.create(create_ws_payload)
-		print("UNPRIVILEGED", created_ws)
-
-	# TODO: populate the unprivileged credentials
-	# TODO: provide the outputs
-
-
-def get_azure_creds(vault_client):
-	# TODO: take the role as a variable
-	azure_config = vault_client.secrets.azure.read_config()
-	azure_creds = vault_client.secrets.azure.generate_credentials(name="my-role")
-
-	subscription_id = azure_config["subscription_id"]
-	tenant_id = azure_config["tenant_id"]
-	client_id = azure_creds["client_id"]
-	client_secret = azure_creds["client_secret"]
-
-	merged_creds_data = [
-		{
-			"key": "ARM_SUBSCRIPTION_ID",
-			"value": subscription_id,
-			"sensitive": False,
-			"hcl": False,
-			"category": "env"
-		},
-		{
-			"key": "ARM_TENANT_ID",
-			"value": tenant_id,
-			"sensitive": True,
-			"hcl": False,
-			"category": "env"
-		},
-		{
-			"key": "ARM_CLIENT_ID",
-			"value": client_id,
-			"sensitive": True,
-			"hcl": False,
-			"category": "env"
-		},
-		{
-			"key": "ARM_CLIENT_SECRET",
-			"value": client_secret,
-			"sensitive": True,
-			"hcl": False,
-			"category": "env"
-		},
-	]
-
-	return merged_creds_data
 
 
 if __name__ == "__main__":
@@ -201,8 +187,14 @@ if __name__ == "__main__":
 	# TODO: why does this take so long?
 	TIMEOUT_TIME=120
 	vault_client = hvac.Client(url=VAULT_ADDR, token=VAULT_TOKEN, timeout=TIMEOUT_TIME)
-	# get_azure_creds(vault_client)
+
+	priv_config = config["privileged"]
+	unpriv_config = config["unprivileged"]
 
 	# TODO: Create the AKS cluster w/ elevated credentials
-	create_privileged_ws(config["privileged"], tfc_client, vault_client)
-	# create_unprivileged_ws(config["unprivileged"], tfc_client, vault_client)
+	priv_ws = create_ws(priv_config, tfc_client, vault_client)
+	priv_ws_id = priv_ws["data"]["id"]
+	populate_tf_vars(priv_ws_id, priv_config)
+	populate_env_vars(priv_ws_id, VAULT_AZURE_PRIV_ROLE)
+
+	# create_ws(config["unprivileged"], tfc_client, vault_client)
