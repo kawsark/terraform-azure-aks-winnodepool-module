12799db17f75f88cbe62d2d4809b44f924fe2c60
kawsark <kawsar@hashicorp.com>
2021-10-04 15:43:41 -0400

    Removing aks-administrators

--- 06-aks-administrators-azure-ad.tf
+++ 06-aks-administrators-azure-ad.tf
@@ -2,12 +2,14 @@ data "azuread_client_config" "current" {}
 
 
 # Create Azure AD Group in Active Directory for AKS Admins
+/*
 resource "azuread_group" "aks_administrators" {
   display_name = "${azurerm_resource_group.aks_rg.name}-cluster-administrators"
   owners       = [data.azuread_client_config.current.object_id]
   # security_enabled = true
   description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-cluster."
 }
+*/
 
 # To fetech current user data
 #data "azuread_user" "azuread_existing_user" {
